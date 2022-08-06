a656fa0c4f35105398443d49ebe43cc5737d803f
Tiago Vaz <tiago-vaz@hotmail.com>
2022-08-06 10:07:12 -0300

    Ajustes

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # RoletaAppK
  
+versionCode = 0.1
+
+versionName = "0.1"
+
+
+# Descrição do projeto
+Aplicativo exemplo utilizado como introdução a Android com Kotlin.
+
+Este aplicativo irá gerar um número aleatório entre 0 e 10 a cada vez que for acionado o botão "RODAR".
--- app/src/main/java/com/example/roletaappk/MainActivity.kt
+++ app/src/main/java/com/example/roletaappk/MainActivity.kt
@@ -11,8 +11,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Instanciando o botão, utilizando o findViewById para recuperar informações do activity_main.xml
         val btnRodarNumero = findViewById(R.id.btnRodar) as Button
 
+        // Criando o evento de Listener de Click para o botão
+        // Mapeando o textView que exibirá o valor sorteado com o findViewById,
+        // feito o sorteio, utilizando o Random() do java utils
+        // e gerando a mensagem com o valor sorteado, que será exibido na tela
         btnRodarNumero.setOnClickListener {
             var texto = findViewById<TextView>(R.id.txtSorteado)
             var numeroSorteado = Random().nextInt(11)
